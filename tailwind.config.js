6c34875dd2df44f4ebbd42cf631456c57cdf911d
Jmr13 <Gamayanjomar@yahoo.com>
2023-07-27 18:56:46 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'zaffre': '#3A59FF',
+      'denim': '#4453A9',
+      'anti-white': '#F2F2F2'
+    },
+  },
+  plugins: [],
+}
